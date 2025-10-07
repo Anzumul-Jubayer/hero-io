f5b220443189b736c1d9aeb5ce87be803dcc6e74
S.M ANZUMUL JUBAYER <anzumuljubayer222@gmail.com>
2025-10-08 01:32:34 +0600

    error-page

--- src/Pages/Errorpage.jsx
+++ src/Pages/Errorpage.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import errorImg from "../assets/error-404.png"
+import { Link } from 'react-router';
 const Errorpage = () => {
     return (
-        <div>
-            <h1>404 not found</h1>
+        <div className='flex flex-col gap-5 justify-center items-center'>
+            <img src={errorImg} alt="" />
+            <h1 className='text-3xl font-bold'>Oops, page not found!</h1>
+            <p className='text-gray-400'>The page you are looking for is not available.</p>
+         <Link
+          to="/home"
+          className="btn text-white bg-linear-to-r from-[#632ee3] to-[#9f62f2]"
+          
+        >
+           Go Back
+        </Link>
         </div>
+
     );
 };
 
